1cce8de76a1b4181df97f7f6e6ed02de6a5e8d5d
namasampath <namasampath240@gmail.com>
2019-12-21 12:24:34 +0530

    testing git revert

--- 1STGo.go
+++ 1STGo.go
@@ -1,5 +1,8 @@
-package main  
-import "fmt"  
-func main() {  
-   fmt.Println("Hello, World")  
-}  
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World")
+	//adding comment
+}
